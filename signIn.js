7369f309f460be0799154124beaf208557003450
eljesakrasniqi <115579853+eljesakrasniqi@users.noreply.github.com>
2024-11-16 19:20:29 +0100

    test
    testest

---
+++ signIn.js
@@ -0,0 +1,10 @@
+
+
+function signInFn(e){
+    e.preventdefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log("email");
+    console.log("password");
+}
+
